e482bdf8b11fcfd64c4367173767548bbfa036f0
dantm16 <dantm16@gmail.com>
2024-11-16 17:03:50 +0000

    script updated to count characters too

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ def main():
     book_path = "books/frankenstein.txt"
     text = full_book(book_path)
     total_words = count_words(text)
-    print(f"The total number of words in this book is {total_words}")
+    total_chars = count_characters(text)
+    print(f"The total number of words in this book is {total_words}.")
+    print("The total number of characters is broken down as follows:")
+    for char in total_chars:
+        print(f"{char}: {total_chars[char]}")
 
     
              
@@ -14,6 +18,14 @@ def full_book(book_path):
     with open(book_path) as f:
         return f.read()    
 
+def count_characters(text):
+    char_count = {}
+    for char in text:
+        if char in char_count:
+            char_count[char] += 1
+        else:
+            char_count[char] = 1
+    return char_count
 
 
 main()
